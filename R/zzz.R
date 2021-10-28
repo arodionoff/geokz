b0c8aef7b343b15b5f81538b388852fd6de0a268
ARodonov <ARodionov@1cb.kz>
2021-10-28 17:17:40 +0600

    Add `.onAttach()` in `R/zzz.R`

---
+++ R/zzz.R
@@ -0,0 +1,20 @@
+#' Prints package startup message
+#'
+#' This function is only called by R when attaching the package.
+#'
+#' @param libname Name of library.
+#' @param pkgname Name of package.
+#' @return Nothing
+.onAttach <- function(libname, pkgname) {
+  geokzVersion <- tryCatch(utils::packageDescription("geokz", fields = "Version"),
+                            warning = function(w) return("alpha"),
+                            error = function(e) return("alpha"))
+  msg <- paste0("geokz ", geokzVersion,
+                "\nAdministrative Boundaries of Kazakhstan as Shapefiles",
+                "\nType citation(\"geokz\") for citing this R package in publications")
+
+  if (!interactive())
+    msg[1] <- paste("Package 'geokz' version", packageVersion("geokz"))
+  packageStartupMessage(msg)
+  invisible()
+}
