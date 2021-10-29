7533d49f0f7e21ddd46481d852ea6da701df1b67
ARodonov <ARodionov@1cb.kz>
2021-10-29 14:12:15 +0600

    Test `sf::st_crs(x = kaz_adm0_sf) <- 4326L`

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -6,22 +6,21 @@ testthat::test_that("geokz::kaz_adm0_sf dataset for Administrative units level 0
   # load(paste0("data/", "kaz_adm0_sf.rda"))
   x <- geokz::kaz_adm0_sf
 
-  # Coordinate Reference System of object
-  # EPSG:4326 or WGS84 - see https://github.com/r-spatial/sf/issues/1419
-  # testthat::expect_silent(sf::st_crs(x = x) <- 4326L)
-  testthat::expect_silent(x <-
-                            sf::st_transform(x = x,
-                                    crs = "+proj=lcc +lon_0=67 +lat_1=45 +lat_2=51 +ellps=krass"))
-
   testthat::expect_true(is.data.frame(x))
   testthat::expect_s3_class(x, "sf")
-
-  testthat::expect_equal(nrow(x), 1L)
-  testthat::expect_equal(sf::st_crs(x)$input, "+proj=lcc +lon_0=67 +lat_1=45 +lat_2=51 +ellps=krass")
   testthat::expect_true(all(sf::st_is_valid(x)))
+
+  # Coordinate Reference System of object
+  # EPSG:4326 or WGS84 - see https://github.com/r-spatial/sf/issues/1419
+  testthat::expect_warning(sf::st_crs(x = x) <- 4326L, NA)
+  testthat::expect_silent(x <-
+                            sf::st_transform(x = x,
+                                             crs = "+proj=lcc +lon_0=67 +lat_1=45 +lat_2=51 +ellps=krass"))
+  testthat::expect_equal(sf::st_crs(x)$input, "+proj=lcc +lon_0=67 +lat_1=45 +lat_2=51 +ellps=krass")
   testthat::expect_silent(x <- sf::st_transform(x = x, crs = "EPSG:4326"))
 
   # structure of datasets...
+  testthat::expect_equal(nrow(x), 1L)
   testthat::expect_equal(ncol(x), 6L)
   testthat::expect_equal(colnames(x), c("KATO", "ADM0_EN", "ADM0_KK", "ADM0_RU", "ADM0_PCODE",
                                         "geometry"))
@@ -42,12 +41,19 @@ testthat::test_that("geokz::kaz_adm1_sf dataset for Administrative units level 1
 
   testthat::expect_true(is.data.frame(x))
   testthat::expect_s3_class(x, "sf")
-
-  testthat::expect_equal(nrow(x), 17L)
-  testthat::expect_equal(sf::st_crs(x)$input, "WGS 84")
   testthat::expect_true(all(sf::st_is_valid(x)))
 
+  # Coordinate Reference System of object
+  # EPSG:4326 or WGS84 - see https://github.com/r-spatial/sf/issues/1419
+  testthat::expect_warning(sf::st_crs(x = x) <- 4326L, NA)
+  testthat::expect_silent(x <-
+                            sf::st_transform(x = x,
+                                             crs = "+proj=lcc +lon_0=67 +lat_1=45 +lat_2=51 +ellps=krass"))
+  testthat::expect_equal(sf::st_crs(x)$input, "+proj=lcc +lon_0=67 +lat_1=45 +lat_2=51 +ellps=krass")
+  testthat::expect_silent(x <- sf::st_transform(x = x, crs = "EPSG:4326"))
+
   # structure of datasets...
+  testthat::expect_equal(nrow(x), 17L)
   testthat::expect_equal(ncol(x), 11L)
   testthat::expect_equal(colnames(x), c("KATO",
                                         "ADM0_EN", "ADM0_KK", "ADM0_RU", "ADM0_PCODE",
@@ -75,12 +81,19 @@ testthat::test_that("geokz::kaz_adm2_sf dataset for Administrative units level 2
 
   testthat::expect_true(is.data.frame(x))
   testthat::expect_s3_class(x, "sf")
-
-  testthat::expect_equal(nrow(x), 216L)
-  testthat::expect_equal(sf::st_crs(x)$input, "WGS 84")
   testthat::expect_true(all(sf::st_is_valid(x)))
 
+  # Coordinate Reference System of object
+  # EPSG:4326 or WGS84 - see https://github.com/r-spatial/sf/issues/1419
+  testthat::expect_warning(sf::st_crs(x = x) <- 4326L, NA)
+  testthat::expect_silent(x <-
+                            sf::st_transform(x = x,
+                                             crs = "+proj=lcc +lon_0=67 +lat_1=45 +lat_2=51 +ellps=krass"))
+  testthat::expect_equal(sf::st_crs(x)$input, "+proj=lcc +lon_0=67 +lat_1=45 +lat_2=51 +ellps=krass")
+  testthat::expect_silent(x <- sf::st_transform(x = x, crs = "EPSG:4326"))
+
   # structure of datasets...
+  testthat::expect_equal(nrow(x), 216L)
   testthat::expect_equal(ncol(x), 15L)
   testthat::expect_equal(colnames(x), c("KATO",
                                         "ADM0_EN", "ADM0_KK", "ADM0_RU", "ADM0_PCODE",
@@ -113,12 +126,19 @@ testthat::test_that("geokz::kaz_cnt1_sf dataset for Centers of Administrative un
 
   testthat::expect_true(is.data.frame(x))
   testthat::expect_s3_class(x, "sf")
-
-  testthat::expect_equal(nrow(x), 18L)
-  testthat::expect_equal(sf::st_crs(x)$input, "WGS 84")
   testthat::expect_true(all(sf::st_is_valid(x)))
 
+  # Coordinate Reference System of object
+  # EPSG:4326 or WGS84 - see https://github.com/r-spatial/sf/issues/1419
+  testthat::expect_warning(sf::st_crs(x = x) <- 4326L, NA)
+  testthat::expect_silent(x <-
+                            sf::st_transform(x = x,
+                                             crs = "+proj=lcc +lon_0=67 +lat_1=45 +lat_2=51 +ellps=krass"))
+  testthat::expect_equal(sf::st_crs(x)$input, "+proj=lcc +lon_0=67 +lat_1=45 +lat_2=51 +ellps=krass")
+  testthat::expect_silent(x <- sf::st_transform(x = x, crs = "EPSG:4326"))
+
   # structure of datasets...
+  testthat::expect_equal(nrow(x), 18L)
   testthat::expect_equal(ncol(x), 15L)
   testthat::expect_equal(colnames(x), c("KATO",
                                         "ADM0_EN", "ADM0_KK", "ADM0_RU", "ADM0_PCODE",
